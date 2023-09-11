654c68174ef29d3831f78c579a3c367a3c8711ea
saif <saifgr7@gmail.com>
2023-09-11 18:31:37 +0100

    day 3 commits

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
+//React component
 
-const header = React.createElement(
-  "h1",
-  {
-    id: "front",
-  },
-  "Hello World"
-);
-//const root = ReactDOM.createRoot(document.getElementById("root"));
+//React element
+const FooterComponent = () => {
+  return (
+    <div>
+      <h2>Lets begin</h2>
+      <h3>Shall we?...</h3>
+    </div>
+  );
+};
+
+
+function spell(word){
+  return word;
+}
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I am Saif</h2>
+      <button>{spell("Can we begin?")}</button>
+      <HeadingComponent />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const footer = React.createElement("h1", {}, "Down we are");
-
-root.render(footer);
-
+//root.render(header); //way to call create element
+root.render(<HeadingComponent />); //way to call react component
