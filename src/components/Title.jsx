754b9aa8276c47a2e97fd1a408c28190e7d2f18b
saif <saifgr7@gmail.com>
2023-09-14 23:02:14 +0100

    tonight changes

---
+++ src/components/Title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <a href="/">
+        <img
+          className="Logo"
+          alt="no img"
+          src="https://azure.wgp-cdn.co.uk/app-food-heaven/categories/FoodHeavenLogo-Large.png?qc-size=233,168"
+        ></img>
+      </a>
+    </div>
+  );
+};
+export default Title;
